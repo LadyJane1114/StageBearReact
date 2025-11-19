14f40eb4f67adef6262fc36fc3177413902b43e1
LadyJane1114 <sweet.jane.s@outlook.com>
2025-11-19 11:10:41 -0400

    added Shakes logo

--- StageBear-ui/src/main.jsx
+++ StageBear-ui/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
-import Dog from './components/Dog.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
