ab506cd0743a2b02a44338c7909b79d77d03fdad
LadyJane1114 <sweet.jane.s@outlook.com>
2025-11-20 14:19:17 -0400

    followed along in class and things are so far working as expected.

--- StageBear-ui/src/App.jsx
+++ StageBear-ui/src/App.jsx
@@ -1,13 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './css/App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  //Define a state variable
+  const [shows, setShows] = useState([])
+
+  //get API Url from env.variables
+  const apiURL = import.meta.env.VITE_API_URL
+
+  //Fetch shows from API when components mount
+  useEffect(() => {
+    const getShows = async () => {
+      const response = await fetch(apiURL)
+      const result = await response.json()
+
+      if(response.ok){
+        setShows(result)
+      }
+    }
+    getShows()
+  }, [])
+
 
   return (
     <>
-      
+      <h1>StageBear</h1>
+
+      {
+        shows.length > 0 && (
+          shows.map(show => (
+            <div key={show.ShowID}>
+              <h2>{show.Title}</h2>
+              
+              {show.Image?.trim().startsWith("http") ? (
+                <img src={show.Image} className="img-fluid img-thumbnail" />
+                    ) : (
+                      <img src="/ShakesPlaceholder.png" />
+                    )}
+              
+              <button>Find Tickets!</button>
+              <br/>
+            </div>
+          ))
+        )
+      }
     </>
   )
 }
