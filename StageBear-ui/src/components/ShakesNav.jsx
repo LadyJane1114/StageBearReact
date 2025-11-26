132df28e45fee4bb17b7bb5f78ff5ca7bba0243d
LadyJane1114 <sweet.jane.s@outlook.com>
2025-11-26 09:52:46 -0400

    working on nav

--- StageBear-ui/src/components/ShakesNav.jsx
+++ StageBear-ui/src/components/ShakesNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Nav, Navbar, Container } from "react-bootstrap"
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap"
 
 const ShakesNav = () => {
   return (
@@ -10,7 +10,25 @@ const ShakesNav = () => {
                 <Navbar.Brand as={Link} to="/">StgBr</Navbar.Brand>
                 <Nav className="d-flex justify-content-center">
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    {/* <Nav.Link as={Link} to="/details:id">Details</Nav.Link> */}
+                      <NavDropdown title="Countries" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Canada</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">
+                          United States
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action5">
+                          United Kingdom
+                        </NavDropdown.Item>
+                      </NavDropdown>
+
+                      <NavDropdown title="Countries" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Canada</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">
+                          United States
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action5">
+                          United Kingdom
+                        </NavDropdown.Item>
+                      </NavDropdown>
                 </Nav>
             </Container>
         </Navbar>
