b408bfb09597dd9c441ce3554778f181d0a7a059
LadyJane1114 <sweet.jane.s@outlook.com>
2025-11-28 00:32:47 -0400

    Footer is good

--- StageBear-ui/src/components/Footer.jsx
+++ StageBear-ui/src/components/Footer.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom' //
+import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Footer = () => {
   return (
     <>
-    <footer className='footer'>
-        <div className='copyright'>&copy; Lady Jane Scott - Nov 2025</div>
-        <div>
-            <h2>Helpful Links</h2>
-            <ul>
-                <li><Link to="/" className="footerlink">Home Page</Link></li>
-                <li><Link to="/aboutUs" className="footerlink">About Us</Link></li>
-                <li><Link to="/faq" className="footerlink">Get Help</Link></li>
-                <li><Link to="/aboutShakes" className="footerlink">Who is Shakes?</Link></li>
-            </ul>
-        </div>
-        <div>
-            <h2></h2>
-        </div>
-    </footer>
+    <Container fluid className='footer'>
+      <Row>
+        <Col sm={8}>
+          <div className='copyright'>
+            <h3>&copy; Lady Jane Scott - Nov 2025</h3>
+          </div>
+        </Col>
+        <Col sm={4}>
+          <div>
+              <h3>Helpful Links</h3>
+              <ul>
+                  <li className><Link to="/" className="footerlink">Home Page</Link></li>
+                  <li><Link to="/aboutUs" className="footerlink">About Us</Link></li>
+                  <li><Link to="/faq" className="footerlink">Get Help</Link></li>
+                  <li><Link to="/aboutShakes" className="footerlink">Who is Shakes?</Link></li>
+              </ul>
+          </div>
+        </Col> 
+      </Row>
+    </Container>
     </>
   )
 }
