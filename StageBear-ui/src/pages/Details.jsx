151083fd63c2b9e1825d66462471acf18b5a6f51
LadyJane1114 <sweet.jane.s@outlook.com>
2025-11-28 15:19:53 -0400

    routine commit, continued with Details page

--- StageBear-ui/src/pages/Details.jsx
+++ StageBear-ui/src/pages/Details.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import { ListGroup } from 'react-bootstrap'
+
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 const Details = ({shows}) => {
     const { id } = useParams()
 
     const show = shows.find(s => s.ShowID === parseInt(id))
 
-    if (!show) {
-        return <p>Show not found.</p>
-    }
+    
 
     const performances = shows
       .filter(s => s.Title === show.Title)
       .sort((a, b) => new Date(a.Scheduled) - new Date(b.Scheduled))
 
-    
+    const [modalVisible, setModalVisible] = useState(false);
+    const handleClose = () => setModalVisible(false);
+    const handleShow = () => setModalVisible(true);
+
+    if (!show) {
+        return <p>Show not found.</p>
+    }
 
     return (
     <>
@@ -29,18 +34,41 @@ const Details = ({shows}) => {
                     ) : (
                       <img src="/ShakesPlaceholder.png" />
                     )}
-      <div className='details-overlay'>
-        <h1 className='show-title'>{show.Title}</h1>
-      </div>
-    </div>
 
-      <div className='show-description'>
-      <h2>About this Show</h2>
-        <p>{show.Description}</p>
-      <br/>
-      <h3>Venue</h3>
-        <p>{show.VenueName}</p>
-    </div>
+          <div className='details-overlay'>
+            <h1 className='show-title'>{show.Title}</h1>
+          </div>
+
+        </div>
+
+        <div className='show-description'>
+            <h2>About this Show</h2>
+              <p>{show.Description}</p>
+            <br/>
+            <h3>Venue</h3>
+              <button onClick={handleShow} className='venue-btn'>{show.VenueName}</button>
+
+              <Modal show={modalVisible} onHide={handleClose}>
+
+                  <Modal.Header closeButton>
+                    <Modal.Title>{show.VenueName}</Modal.Title>
+                  </Modal.Header>
+
+                  <Modal.Body>
+                    <p>Address: {show.StreetAddress}, {show.City} {show.Region}, {show.Country}</p>
+                    <p>Phone: +{show.VenuePhone}</p>
+                    <br/>
+                    <h5>About this Venue</h5>
+                    <p>{show.VenueNotes}</p>
+                  </Modal.Body>
+
+                  <Modal.Footer>
+                    <button variant="secondary" onClick={handleClose}>
+                      Close
+                    </button>
+                  </Modal.Footer>
+            </Modal>
+        </div>
       </div>
 
       <div className='details-right'>
